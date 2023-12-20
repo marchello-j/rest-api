40465a8ed5b415b48509b28c07c9c00d25eb47cb
Marsel <marchello.marsel@gmail.com>
2023-12-20 20:13:19 +0300

    add route delete for db

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response } from 'express';
+
 import { videoRoute } from './routes/video-route';
 import { blogRoute } from './routes/blog-route';
 import { postRoute } from './routes/post-route';
+import { db } from './db/db';
 
 export const app = express();
 app.use(express.json());
@@ -9,3 +11,9 @@ app.use(express.json());
 app.use('/videos', videoRoute);
 app.use('/blogs', blogRoute);
 app.use('/posts', postRoute);
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+	db.blogs.length = 0;
+	db.posts.length = 0;
+	db.videos.length = 0;
+	res.sendStatus(204);
+});
